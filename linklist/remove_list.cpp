4b0d71ac3cb75ff98986468da2d50f372e9908ad
sosqwq <zhenshuo200164@gmail.com>
2024-11-11 17:06:28 -0800

    Create remove_list.cpp

---
+++ linklist/remove_list.cpp
@@ -0,0 +1,50 @@
+// include list
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+ struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ };
+ 
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        while ( head != NULL && head->val == val){
+            ListNode* temp = head;
+            head = head -> next;
+            delete temp;
+        }
+
+        ListNode* current = head;
+
+        while ( current != nullptr && current->next != nullptr){
+            if (current->next->val == val){
+                ListNode* temp = current -> next;
+                current -> next = current -> next -> next;
+                delete temp;
+            }
+            else{
+                current = current ->next;
+            }
+        }
+
+        return head;
+    }
+};

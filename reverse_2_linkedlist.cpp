305ad0dd448c8a28df25c636abf066e23204481a
sosqwq <zhenshuo200164@gmail.com>
2024-11-11 22:09:29 -0800

    Update reverse_2_linkedlist.cpp

--- reverse_2_linkedlist.cpp
+++ reverse_2_linkedlist.cpp
@@ -14,6 +14,25 @@ using namespace std;
 class Solution {
 public:
     ListNode* reverseBetween(ListNode* head, int left, int right) {
+        if (left == right){
+            return head;
+        }
+        ListNode* dummy = new ListNode(0); // Dummy node to simplify edge cases
+        dummy->next = head;
+        ListNode* prev = dummy;
+        //prev 是 current 的前一个节点，因此 left -1;
+        for(int i = 0; i< left -1; i++){
+            prev = prev -> next;
+        } 
         
+        ListNode* current = prev -> next;
+        ListNode* temp = current->next;
+        for (int i = left; i < right; i++) {
+            current->next = temp->next;
+            temp->next = prev->next;
+            prev->next = temp;
+            temp = current->next;
+        }
+        return dummy->next;
     }
 };

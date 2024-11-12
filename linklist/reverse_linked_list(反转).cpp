30c40ccc780699202e91b5cc19f7cf4f159ce30d
sosqwq <zhenshuo200164@gmail.com>
2024-11-11 19:35:44 -0800

    Create reverse_linked_list(反转).cpp

---
+++ linklist/reverse_linked_list(反转).cpp
@@ -0,0 +1,31 @@
+// 206  reverse_linked_list（反转）.cpp
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+ struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ };
+
+ class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        ListNode * current = head;
+        ListNode * prev = nullptr;
+        ListNode * temp;
+
+        while (current != nullptr){
+            temp = current -> next;
+            current -> next = prev;
+            prev = current;
+            current = temp;
+        }
+        return prev;
+    }
+};

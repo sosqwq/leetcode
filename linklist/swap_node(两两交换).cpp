de5ce01f6590b256208eaa0866b42c439ac663ce
sosqwq <zhenshuo200164@gmail.com>
2024-11-11 18:21:05 -0800

    Create swap_node(两两交换).cpp

---
+++ linklist/swap_node(两两交换).cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// 24. Swap Nodes in Pairs
+ struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ };
+
+ class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        
+    }
+};

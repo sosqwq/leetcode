5d46ca769d40102e92f2c5ec6f7a6716a69e3de7
sosqwq <zhenshuo200164@gmail.com>
2024-11-11 19:37:16 -0800

    Create reverse_2_linkedlist.cpp

---
+++ reverse_2_linkedlist.cpp
@@ -0,0 +1,19 @@
+// 92. Reverse Linked List II
+#include <iostream>
+#include <vector>
+using namespace std;
+
+ struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ };
+
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+        
+    }
+};

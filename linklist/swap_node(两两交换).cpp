6ff7b3468fc3bf19042ce289a2afeb58227676a2
sosqwq <zhenshuo200164@gmail.com>
2024-11-14 20:37:39 -0800

    Update swap_node(两两交换).cpp

--- linklist/swap_node(两两交换).cpp
+++ linklist/swap_node(两两交换).cpp
@@ -15,6 +15,29 @@ using namespace std;
  class Solution {
 public:
     ListNode* swapPairs(ListNode* head) {
-        
+        ListNode *dummy = new ListNode(0);
+        dummy->next = head;
+        ListNode *current = dummy;
+
+        ListNode *temp;
+        ListNode *temp1;
+        while (current -> next != nullptr && current -> next -> next != nullptr){
+            temp = current ->next;
+            temp1 = current -> next ->next ->next;
+
+            current -> next = current -> next -> next;
+            current -> next -> next = temp;
+            temp -> next = temp1;
+ 
+            current = current -> next -> next;
+
+        }
+        return dummy -> next;
+
+
+
+
+
+
     }
 };

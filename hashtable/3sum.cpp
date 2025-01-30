17855b0692b40fdb859c3a3d6f71eb4ece364a02
sosqwq <zhenshuo200164@gmail.com>
2025-01-29 22:34:15 -0800

    1/29

--- hashtable/3sum.cpp
+++ hashtable/3sum.cpp
@@ -5,4 +5,50 @@
 #include <cstring> 
 #include <unordered_set>
 #include <unordered_map>
+#include <algorithm>
+
 using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> result;
+        sort(nums.begin(),nums.end());
+
+        for(int i = 0;  i< nums.size();i++){
+            if(nums[i]> 0){
+                return result;  
+            }
+            if (i> 0 && nums[i-1]){
+                continue;
+            }
+            // 双指针
+            int left = i+1;
+            int right = nums.size() -1;
+
+            while(right> left){
+                if(nums[1]+ nums[left]+ nums[right] > 0){
+                    right --;
+                }
+                else if(nums[1]+ nums[left]+ nums[right] <0){
+                    left ++;
+                }
+                else {
+                    result.push_back(vector <int> {nums[i], nums[left], nums[right]});
+                    //去重
+                    while( right > left && nums[right] == nums[right -1]){
+                        right --;
+                    }
+
+                    while(right > left && nums[left] == nums[left +1]){
+                        left++;
+                    }
+                    right --;
+                    left ++;
+
+                }
+            }
+        }
+        return result;
+    }
+};

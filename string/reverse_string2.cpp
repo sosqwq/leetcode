31c0275eb129387fb5fbf97eaee7835ca44b291c
sosqwq <zhenshuo200164@gmail.com>
2025-01-30 19:33:57 -0800

    Update reverse_string2.cpp

--- string/reverse_string2.cpp
+++ string/reverse_string2.cpp
@@ -12,10 +12,10 @@ public:
     string reverseStr(string s, int k) {
         for(int i= 0; i< s.size(); i+= 2*k){
             if(i+k < s.size()){
-                reverse(s[i],s[i+k]);
+                reverse(s.begin() + i, s.begin() + i + k);
             }
             else{
-                reverse(s[i], s[s.size()]);
+                reverse(s.begin() + i, s.end());
             }
         }
     }

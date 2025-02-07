b9f395a3f256c14e99cfb44ca467e3fba8a9a4a0
sosqwq <zhenshuo200164@gmail.com>
2025-02-06 16:10:45 -0800

    2/6

---
+++ queue_and_stacks/implement_queue_using_stacks.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <cstring> 
+#include <unordered_set>
+#include <unordered_map>
+#include <algorithm>
+using namespace std;
+
+class MyQueue {
+public:
+    MyQueue() {
+        
+    }
+    
+    void push(int x) {
+        
+    }
+    
+    int pop() {
+        
+    }
+    
+    int peek() {
+        
+    }
+    
+    bool empty() {
+        
+    }
+};

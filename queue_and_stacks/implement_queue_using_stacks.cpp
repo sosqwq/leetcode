1c91696e155a3ba90194f6e13682c507598e9c47
sosqwq <zhenshuo200164@gmail.com>
2025-02-09 23:07:32 -0800

    Update implement_queue_using_stacks.cpp

--- queue_and_stacks/implement_queue_using_stacks.cpp
+++ queue_and_stacks/implement_queue_using_stacks.cpp
@@ -7,24 +7,43 @@
 using namespace std;
 
 class MyQueue {
-public:
-    MyQueue() {
-        
-    }
+    public:
+        stack<int> stIn;
+        stack<int> stOut;
+        /** Initialize your data structure here. */
+        MyQueue() {
     
-    void push(int x) {
-        
-    }
+        }
+        /** Push element x to the back of queue. */
+        void push(int x) {
+            stIn.push(x);
+        }
     
-    int pop() {
-        
-    }
+        /** Removes the element from in front of queue and returns that element. */
+        int pop() {
+            // 只有当stOut为空的时候，再从stIn里导入数据（导入stIn全部数据）
+            if (stOut.empty()) {
+                // 从stIn导入数据直到stIn为空
+                while(!stIn.empty()) {
+                    stOut.push(stIn.top());
+                    stIn.pop();
+                }
+            }
+            int result = stOut.top();
+            stOut.pop();
+            return result;
+        }
     
-    int peek() {
-        
-    }
+        /** Get the front element. */
+        int peek() {
+            int res = this->pop(); // 直接使用已有的pop函数
+            stOut.push(res); // 因为pop函数弹出了元素res，所以再添加回去
+            return res;
+        }
     
-    bool empty() {
-        
-    }
-};
+        /** Returns whether the queue is empty. */
+        bool empty() {
+            return stIn.empty() && stOut.empty();
+        }
+    };
+    

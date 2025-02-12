19ee36731b5cc478698a5d5d8e790ad7e7cdab50
sosqwq <zhenshuo200164@gmail.com>
2025-02-11 22:00:07 -0800

    Update implement_queue_using_stacks.cpp

--- queue_and_stacks/implement_queue_using_stacks.cpp
+++ queue_and_stacks/implement_queue_using_stacks.cpp
@@ -4,46 +4,47 @@
 #include <unordered_set>
 #include <unordered_map>
 #include <algorithm>
+#include <stack>
 using namespace std;
 
 class MyQueue {
     public:
-        stack<int> stIn;
-        stack<int> stOut;
-        /** Initialize your data structure here. */
+        stack<int> stackIn;
+        stack<int> stackOut;
+
         MyQueue() {
-    
+            
         }
-        /** Push element x to the back of queue. */
+        
         void push(int x) {
-            stIn.push(x);
+            stackIn.push(x);
         }
-    
-        /** Removes the element from in front of queue and returns that element. */
+        
         int pop() {
-            // 只有当stOut为空的时候，再从stIn里导入数据（导入stIn全部数据）
-            if (stOut.empty()) {
-                // 从stIn导入数据直到stIn为空
-                while(!stIn.empty()) {
-                    stOut.push(stIn.top());
-                    stIn.pop();
+            if(stackOut.empty()){
+                while(!stackIn.empty()){
+                    stackOut.push(stackIn.top());
+                    stackIn.pop();
                 }
             }
-            int result = stOut.top();
-            stOut.pop();
+            int result = stackOut.top();
+            stackOut.pop();
             return result;
         }
-    
-        /** Get the front element. */
+        
         int peek() {
-            int res = this->pop(); // 直接使用已有的pop函数
-            stOut.push(res); // 因为pop函数弹出了元素res，所以再添加回去
-            return res;
+            if(stackOut.empty()){
+                while(!stackIn.empty()){
+                    stackOut.push(stackIn.top());
+                    stackIn.pop();
+                }
+            }
+            int result = stackOut.top();
+            return result;
         }
-    
-        /** Returns whether the queue is empty. */
+        
         bool empty() {
-            return stIn.empty() && stOut.empty();
+            return stackIn.empty()&& stackOut.empty();
         }
     };
     

4809eb81b6e15078a0d7157b0e430eabfd0d9e0b
sosqwq <zhenshuo200164@gmail.com>
2025-02-15 21:08:23 -0800

    Create sliding_window_maximum.cpp

---
+++ queue_and_stacks/sliding_window_maximum.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <cstring> 
+#include <unordered_set>
+#include <unordered_map>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+    public:
+        vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+            //没搞定，明天写
+        }
+    };

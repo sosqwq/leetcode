1b3b292a45520c402c160a78d6c298227738b7e4
sosqwq <zhenshuo200164@gmail.com>
2025-02-16 23:14:28 -0800

    Update sliding_window_maximum.cpp

--- queue_and_stacks/sliding_window_maximum.cpp
+++ queue_and_stacks/sliding_window_maximum.cpp
@@ -4,11 +4,49 @@
 #include <unordered_set>
 #include <unordered_map>
 #include <algorithm>
+#include <deque>
 using namespace std;
 
 class Solution {
+    private:
+    class MyQueue { //单调队列（从大到小）
+        public:
+            deque <int> que; 
+            void pop(int val){
+                if(!que.empty()&& val==que.front()){
+                    que.pop_front();
+                }
+
+            }
+            void push(int val){
+                while(!que.empty() && val > que.back()){
+                    que.pop_back();
+                }
+                que.push_back(val);
+
+            }
+            int front(){
+                return que.front();
+            }
+        };
+            
     public:
+        
         vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-            //没搞定，明天写
+            MyQueue que;
+            vector<int> result;
+            // 先将k元素放进队列
+            for(int i= 0; i<k;i++){
+                que.push(nums[i]);
+                result.push_back(que.front());
+            }
+            //从K开始
+            for(int i = k; i< nums.size();i++){
+                que.pop(nums[i-k]);
+                que.push(nums[i]);
+                result.push_back(que.front());
+            }
+            return result;
+
         }
     };

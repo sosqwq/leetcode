58e3906b117bcba61b0ab8eee4c5f1b087b87d5d
sosqwq <zhenshuo200164@gmail.com>
2025-02-20 18:39:23 -0800

    2/20

--- binary_tree/invert_binary_tree.cpp
+++ binary_tree/invert_binary_tree.cpp
@@ -9,7 +9,23 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+#include <iostream>
+#include <vector>
+#include <cstring> 
+#include <unordered_set>
+#include <unordered_map>
+#include <algorithm>
+#include <stack>
+#include <queue>
+using namespace std;
 
+struct TreeNode {
+    int val;
+    TreeNode * left;
+    TreeNode * right;
+    TreeNode (): val(0),left(nullptr), right(nullptr){}
+
+};
 
 class Solution {
     public:

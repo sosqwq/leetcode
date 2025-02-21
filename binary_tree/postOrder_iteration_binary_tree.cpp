5570279e7e5e2222842d4f2f0057719b117879bd
sosqwq <zhenshuo200164@gmail.com>
2025-02-21 10:41:38 -0800

    Create postOrder_iteration_binary_tree.cpp

---
+++ binary_tree/postOrder_iteration_binary_tree.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <cstring> 
+#include <unordered_set>
+#include <unordered_map>
+#include <algorithm>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+struct TreeNode{
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(): val(0),left(nullptr),right(nullptr){
+
+    }
+};
+
+class Solution {
+    public:
+        vector<int> postorderTraversal(TreeNode* root) {
+            stack<TreeNode*> st;
+            vector<int> result;
+            if(root == NULL) return result;
+            st.push(root);
+
+            while(!st.empty()){
+                TreeNode * node = st.top();
+                result.push_back(node-> val);
+                st.pop();
+                if(node-> left) st.push(node-> left);
+                if(node-> right) st.push(node-> right);
+
+            }
+            reverse(result.begin(),result.end());
+            return result;
+
+            
+
+        }
+    };

142ec21242caab4ecd946bb62a56f33756309665
sosqwq <zhenshuo200164@gmail.com>
2025-02-21 09:58:44 -0800

    Create preOrder_iteration.cpp

---
+++ binary_tree/preOrder_iteration.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <cstring> 
+#include <unordered_set>
+#include <unordered_map>
+#include <algorithm>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+struct TreeNode{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode (): val(0),left(nullptr),right(nullptr){}
+};
+class Solution {
+    public:
+        vector<int> preorderTraversal(TreeNode* root) {
+
+        }
+    };

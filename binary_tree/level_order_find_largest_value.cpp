2dc6bcfabbf634b4a032e26cb9e57a57c27fce5a
sosqwq <zhenshuo200164@gmail.com>
2025-02-23 13:02:44 -0800

    Update level_order_find_largest_value.cpp

--- binary_tree/level_order_find_largest_value.cpp
+++ binary_tree/level_order_find_largest_value.cpp
@@ -9,3 +9,37 @@
 
 using namespace std;
 
+struct TreeNode{
+    int val;
+    TreeNode * left;
+    TreeNode * right;
+    TreeNode ():val(0), left(nullptr),right(nullptr){}
+};
+
+class Solution {
+    public:
+        vector<int> largestValues(TreeNode* root) {
+            queue<TreeNode *> que;
+            vector<int> result;
+
+            if(root != NULL) que.push(root);
+
+
+            while(! que.empty()){
+                int size = que.size();
+                int largest =0;
+
+                while(size--){
+                    TreeNode * node = que.front();
+                    if(largest < node ->val){
+                        largest = node -> val;
+                    }
+                    if(node -> left) que.push(node-> left);
+                    if(node -> right) que.push(node-> right);
+                }
+                result.push_back(largest);
+            }
+
+            return result;
+        }
+    };

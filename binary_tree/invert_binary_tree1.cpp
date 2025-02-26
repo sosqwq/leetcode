fd1123d68f11959a34a0f28e83c6037d1d049eec
sosqwq <zhenshuo200164@gmail.com>
2025-02-25 22:39:37 -0800

    Update invert_binary_tree1.cpp

--- binary_tree/invert_binary_tree1.cpp
+++ binary_tree/invert_binary_tree1.cpp
@@ -19,9 +19,9 @@ struct TreeNode{
 class Solution {
     public:
         TreeNode* invertTree(TreeNode* root) {
-            if(root = NULL) return root  ;
+            if(root == NULL) return root  ;
 
-            swap(root -> left, root -> right);
+            swap(root-> left, root-> right);
             invertTree (root ->left);
             invertTree(root -> right);
             return root;

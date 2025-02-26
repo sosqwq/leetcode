be9a4a464321a8482cb94cf66977ab5823a08fb3
sosqwq <zhenshuo200164@gmail.com>
2025-02-25 21:30:26 -0800

    Create invert_binary_tree1.cpp

---
+++ binary_tree/invert_binary_tree1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <cstring> 
+#include <unordered_set>
+#include <unordered_map>
+#include <algorithm>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+struct TreeNode{
+    int val;
+    TreeNode * left;
+    TreeNode * right;
+    TreeNode(): val(0),left(nullptr),right(nullptr){}
+};
+
+class Solution {
+    public:
+        TreeNode* invertTree(TreeNode* root) {
+            if(root = NULL) return root  ;
+
+            swap(root -> left, root -> right);
+            invertTree (root ->left);
+            invertTree(root -> right);
+            return root;
+        }
+       
+    };

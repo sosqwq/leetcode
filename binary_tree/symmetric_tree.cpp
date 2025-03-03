44df511ea3936a8fd9c91615a8f9695f32efb41a
sosqwq <zhenshuo200164@gmail.com>
2025-03-03 09:55:47 -0800

    Update symmetric_tree.cpp

--- binary_tree/symmetric_tree.cpp
+++ binary_tree/symmetric_tree.cpp
@@ -18,7 +18,22 @@ struct TreeNode{
 
 class Solution {
     public:
+    bool compare (TreeNode * left, TreeNode * right){
+        if (left == NULL && right == NULL) return true;
+        else if (left != NULL && right ==NULL) return false;
+        else if (left == NULL && right != NULL) return false;
+
+        else if (left -> val != right -> val) return false;
+
+        bool outside = compare( left -> left , right -> right);
+        bool inside = compare (left -> right, right -> left);
+        bool isSame = outside && inside;
+
+        return isSame;
+
+    }
         bool isSymmetric(TreeNode* root) {
-            
+            if(root == NULL) return true;
+            return compare(root -> left, root -> right);
         }
     };

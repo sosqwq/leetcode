2489f0c1514fdbe21ad118742e9a113054375deb
sosqwq <zhenshuo200164@gmail.com>
2025-03-03 10:56:05 -0800

    Create count_complete_tree_node.cpp

---
+++ binary_tree/count_complete_tree_node.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <cstring> 
+#include <unordered_set>
+#include <unordered_map>
+#include <algorithm>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+
+struct TreeNode {
+    int value;
+    TreeNode * left;
+    TreeNode * right;
+
+    TreeNode():value(0),left(nullptr),right(nullptr){}
+
+};
+
+class Solution {
+    public:
+        int countNodes(TreeNode* root) {
+            
+        }
+    };

9f13f68c2737b80e1ce0eff4cb7ede05010ec2fd
sosqwq <zhenshuo200164@gmail.com>
2025-03-08 11:52:11 -0800

    Update maximum_binart_tree.cpp

--- binary_tree/maximum_binart_tree.cpp
+++ binary_tree/maximum_binart_tree.cpp
@@ -14,10 +14,45 @@ struct TreeNode{
     TreeNode * left;
     TreeNode * right;
     TreeNode(): val(0),left(nullptr),right(nullptr){}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 class Solution {
     public:
         TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
+            //找最大
+            //分割数组
+            // 如果在左边就是左
+            // 递归
+            TreeNode * root = new TreeNode(0);
+            if(nums.size() == 0) return root;
+
+            if(nums.size() == 1){
+                root -> val = nums[0];
+                return root;
+            }
+
+            int maxval =0;
+            int maxindex = 0;
+            for(int index = 0;index< nums.size();index++){
+        
+                if(maxval< nums[index]){
+                    maxval = nums[index];
+                    maxindex = index;
+                }
+            }
+            root -> val = maxval;
             
+            if(maxindex > 0){
+                vector<int> leftNums(nums.begin(),nums.begin()+ maxindex);
+                root -> left = constructMaximumBinaryTree(leftNums);
+            }
+            if(maxindex < nums.size()-1){
+                vector<int> rightNums(nums.begin() + maxindex + 1, nums.end());
+                root -> right = constructMaximumBinaryTree(rightNums);
+            }
+            return root;
+
+
         }
     };

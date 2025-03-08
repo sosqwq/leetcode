f695473acb5c4e99b3b1660fbf29e28229312301
sosqwq <zhenshuo200164@gmail.com>
2025-03-07 22:48:02 -0800

    Create maximum_binart_tree.cpp

---
+++ binary_tree/maximum_binart_tree.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <cstring> 
+#include <unordered_set>
+#include <unordered_map>
+#include <algorithm>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+struct TreeNode{
+    int val;
+    TreeNode * left;
+    TreeNode * right;
+    TreeNode(): val(0),left(nullptr),right(nullptr){}
+};
+class Solution {
+    public:
+        TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
+            
+        }
+    };

c6f907ac6552fc4fb6d0c78e1a22834d42d9628e
sosqwq <zhenshuo200164@gmail.com>
2025-03-19 23:01:41 -0700

    Update letter_combinations_of_a_phone_number.cpp

--- backtracking/letter_combinations_of_a_phone_number.cpp
+++ backtracking/letter_combinations_of_a_phone_number.cpp
@@ -12,9 +12,44 @@ using namespace std;
 
 class Solution {
     public:
-    vector<int> path;
-    vector<vector<int>> result;
-        vector<string> letterCombinations(string digits) {
-            
+    string s;
+    vector<string> result;
+    string letterMap[10] = {
+        "", 
+        "", 
+        "abc",
+        "def", 
+        "ghi", 
+        "jkl", 
+        "mno",
+        "pqrs",
+        "tuv",
+        "wxyz"
+    };
+    void backtracking(string digits, int index){
+        if(index == digits.size()){
+            result.push_back(s);
+            return ;
+        }
+
+        int digit = digits[index] - '0';
+        string letter = letterMap[digit];
+
+        for(int i= 0;i<letter.size();i++){
+            s.push_back(letter[i]);
+            backtracking(digits,index +1);
+            s.pop_back();
+        }
+
+
+    }
+    vector<string> letterCombinations(string digits) {
+            s.clear();
+            result.clear();
+            if (digits.size() == 0) {
+                return result;
+            }
+            backtracking(digits,0);
+            return result;
         }
     };

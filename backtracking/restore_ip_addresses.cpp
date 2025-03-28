d86ae6bcdc75b5694e06f48eba1b837023f96975
sosqwq <zhenshuo200164@gmail.com>
2025-03-28 09:04:53 -0700

    Create restore_ip_addresses.cpp

---
+++ backtracking/restore_ip_addresses.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <cstring> 
+#include <unordered_set>
+#include <unordered_map>
+#include <algorithm>
+#include <stack>
+#include <queue>
+
+using namespace std;
+class Solution {
+    public:
+    vector<string> result;
+    void backtraking(string s, int starIndex , int pointsum){
+        if(pointsum == 3){
+            if(isValid(s, starIndex, s.size() -1 )){
+                result.push_back(s);
+            }
+        } 
+        for(int i = starIndex; i< s.size(); i++){
+                if(isValid(s, starIndex, i)){
+                    s.insert(s.begin() + i +1 , '.');
+                    pointsum += 1;
+                    backtraking(s, starIndex +2, pointsum);
+                    pointsum -= 1;
+                    s.erase(s.begin()+1+i);
+                }
+                else break;
+            }
+    }
+    bool isValid(const string & s, int start ,int end){
+        if(start > end) return false;
+
+        if( s[start] == '0' && start != end){
+            return false;
+        }
+
+        int num = 0;
+
+        for(int i = start; i<= end; i++){
+            if(s[i] > '9' || s[i] < '0'){// 遇到非数字字符不合法
+                return false;
+            }
+            // 写出完整的数值
+            num = num * 10 +(s[i] -'0');
+            if(num > 255){
+                return false;
+            }
+        }
+        return true;
+    }
+    vector<string> restoreIpAddresses(string s) {
+            backtraking(s, 0,0);
+            return result;
+        }
+    };

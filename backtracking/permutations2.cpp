771ca4f7e336119fa306bbd5decf7068e840e102
sosqwq <zhenshuo200164@gmail.com>
2025-03-31 22:48:59 -0700

    Update permutations2.cpp

--- backtracking/permutations2.cpp
+++ backtracking/permutations2.cpp
@@ -10,7 +10,30 @@
 using namespace std;
 class Solution {
     public:
-        vector<vector<int>> permuteUnique(vector<int>& nums) {
-            
+    vector<int> path;
+    vector<vector<int>> result;
+    void backtracking (vector<int> & nums, vector<bool> used){
+        if(path.size() == nums.size()){
+            result.push_back(path);
+            return;
         }
-    };
+        for(int i = 0;i< nums.size();i++){
+            if(i>0&& nums[i] == nums[i-1] && used [i-1] == false){
+                continue;
+            }
+            
+            path.push_back(nums[i]);
+            used[i] = true;
+            backtracking(nums, used);
+            path.pop_back();
+            used[i] = false;
+        }
+    }
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector<bool> used(nums.size(), false);
+        sort(nums.begin(), nums.end());
+        backtracking(nums, used);
+        return result;
+
+    }
+};

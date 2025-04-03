15099cbcd8c3afbf8047fadcc740c8c02a53dce5
sosqwq <zhenshuo200164@gmail.com>
2025-04-02 22:22:29 -0700

    Update n-queen.cpp

--- backtracking/n-queen.cpp
+++ backtracking/n-queen.cpp
@@ -10,7 +10,45 @@
 using namespace std;
 class Solution {
     public:
+        vector<vector<string>> result;
+        vector<string> chessboard;
+        bool isValid(int row, int col, vector<string> & chessboard){
+            for(int i=0; i< row; i++){
+                if(chessboard [i][col] == 'Q'){
+                    return false;
+                }
+            }
+
+            for(int i= row -1,  j = col -1; i >= 0 && j >= 0; i--,j--){
+                if(chessboard[i][j] == 'Q'){
+                    return false;
+                }
+            }
+
+            for(int i= row -1 , j = col +1; i >= 0 && j >= 0; i--,j++){
+                if(chessboard [i][j] == 'Q'){
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        void backtracking(int n, int row, vector<string>& chessboard){
+            if(row == n){
+                result.push_back(chessboard);
+            }
+
+            for(int col = 0; col < n; col++){
+                if(isValid(row,col,chessboard)){
+                    chessboard[row][col] = 'Q';
+                    backtracking(n,row +1, chessboard);
+                    chessboard[row][col] = '.';
+                }
+            }
+        }
         vector<vector<string>> solveNQueens(int n) {
-            
+            vector<string> chessboard(n, string(n,'.'));
+            backtracking(n,0,chessboard);
+            return result;
         }
     };

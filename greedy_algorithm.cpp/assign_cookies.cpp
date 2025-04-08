a8932e45a94ce2dca9dd4fabd60e23b8bf93804b
sosqwq <zhenshuo200164@gmail.com>
2025-04-07 20:11:13 -0700

    Create assign_cookies.cpp

---
+++ greedy_algorithm.cpp/assign_cookies.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <cstring> 
+#include <unordered_set>
+#include <unordered_map>
+#include <algorithm>
+#include <stack>
+#include <queue>
+
+using namespace std;
+class Solution {
+    public:
+        int findContentChildren(vector<int>& g, vector<int>& s) {
+            
+        }
+    };

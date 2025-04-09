d79dfb5d61b7a55f366249a78620617592fcff65
sosqwq <zhenshuo200164@gmail.com>
2025-04-08 21:45:18 -0700

    Update assign_cookies.cpp

--- greedy_algorithm.cpp/assign_cookies.cpp
+++ greedy_algorithm.cpp/assign_cookies.cpp
@@ -11,6 +11,18 @@ using namespace std;
 class Solution {
     public:
         int findContentChildren(vector<int>& g, vector<int>& s) {
-            
+            sort(s.begin(), s.end());
+            sort(g.begin(), g.end());
+            int index = s.size()-1;
+            int result = 0;
+            for(int i = g.size()-1; i>= 0; i--){
+                if( index >= 0 && s[index] >= g[i] ){
+                    index--;
+                    result ++;
+                }
+            }
+
+            return result;
+
         }
     };

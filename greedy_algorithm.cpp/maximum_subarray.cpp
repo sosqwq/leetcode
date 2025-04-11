5041e3e3a893e35bd1429d808ef0224bb7048028
sosqwq <zhenshuo200164@gmail.com>
2025-04-10 22:05:17 -0700

    Update maximum_subarray.cpp

--- greedy_algorithm.cpp/maximum_subarray.cpp
+++ greedy_algorithm.cpp/maximum_subarray.cpp
@@ -12,6 +12,15 @@ using namespace std;
 class Solution {
     public:
         int maxSubArray(vector<int>& nums) {
-            
+            int result = INT_MIN;
+            int count = 0;
+            for(int i =0; i< nums.size(); i++){
+                count += nums[i];
+                if(count > result) result = count;
+                if(count < 0) count = 0;
+
+            }
+
+            return result;
         }
     };

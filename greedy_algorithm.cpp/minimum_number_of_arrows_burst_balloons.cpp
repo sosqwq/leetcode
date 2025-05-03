939f3cda4d2a7c5df418917473595a12891d6d35
sosqwq <zhenshuo200164@gmail.com>
2025-05-02 20:18:03 -0700

    Create minimum_number_of_arrows_burst_balloons.cpp

---
+++ greedy_algorithm.cpp/minimum_number_of_arrows_burst_balloons.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <cstring> 
+#include <unordered_set>
+#include <unordered_map>
+#include <algorithm>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+class Solution {
+    private:
+    static bool cmp(const vector<int> & a, const vector<int>  & b){
+        return a[0] < b[0];
+    }
+    public:
+        int findMinArrowShots(vector<vector<int>>& points) {
+            if(points.size() == 0) return 0;
+            sort(points.begin(), points.end(), cmp);
+
+            int result = 1;
+            for(int i = 1; i< points.size(); i++){
+                if(points[i][0] > points[i-1][1]){
+                    result += 1;
+                }
+                else{
+                    points[i][1] = min(points[i][1], points[i-1][1]);
+                }
+            }
+            return result;
+
+
+            
+        }
+    };

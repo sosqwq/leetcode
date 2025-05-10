f3eb6387a472750acea26d7281a63a57e9f2472a
sosqwq <zhenshuo200164@gmail.com>
2025-05-09 21:42:36 -0700

    Create climbing_stairs.cpp

---
+++ DP/climbing_stairs.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <cstring> 
+#include <unordered_set>
+#include <unordered_map>
+#include <algorithm>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n<= 2) return n;
+        vector<int> dp(n+1);
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i = 3; i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+
+        }
+        return  dp[n];
+    }
+};

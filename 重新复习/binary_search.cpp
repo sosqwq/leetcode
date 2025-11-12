9805fdb5bf154c31d60a94d7804222fa461b4db5
sosqwq <57452001+sosqwq@users.noreply.github.com>
2025-11-11 22:34:22 -0600

    Update binary_search.cpp

--- 重新复习/binary_search.cpp
+++ 重新复习/binary_search.cpp
@@ -1,6 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <cstring> 
+#include <unordered_set>
+#include <unordered_map>
+#include <algorithm>
+using namespace std;
+
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        
+        int right  = nums.size()-1;
+        int left = 0;
+        int middle;
+        while (left <= right){
+            middle = (left + right) /2;
+            if(nums[middle] < target){
+                left = middle +1;
+            }
+            else if( nums[middle] > target){
+                right = middle -1;
+            }
+            else {
+                return middle;
+            }
+        }
+        return -1;
     }
 };

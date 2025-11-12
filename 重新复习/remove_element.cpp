96c0c3506f4819ff357722fcb91685f7f42d0d12
sosqwq <57452001+sosqwq@users.noreply.github.com>
2025-11-11 22:34:30 -0600

    Create remove_element.cpp

---
+++ 重新复习/remove_element.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <cstring> 
+#include <unordered_set>
+#include <unordered_map>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int slow = 0;
+        for(int fast =0; fast < nums.size(); fast ++){
+            if(nums[fast] != val){
+                nums[slow] = nums[fast];
+                slow ++;
+            }
+        } 
+        return slow;
+    }
+   
+};

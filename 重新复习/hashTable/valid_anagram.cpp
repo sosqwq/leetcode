a7ab56144824a8585c41d5efd8c6e02fb06f6a00
sosqwq <57452001+sosqwq@users.noreply.github.com>
2026-01-16 21:32:37 -0600

    Create valid_anagram.cpp

---
+++ 重新复习/hashTable/valid_anagram.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int arr[26] = {0};
+        if(s.size() != t.size()){
+            return false;
+        }
+        for(int i = 0; i< s.size();i++){
+            arr[s[i]-'a']++;
+            arr[t[i]-'a']--;
+        }
+
+        for(int i= 0;i < 26;i++){
+            if(arr[i] != 0) return false;
+        }
+        return true;
+ 
+        
+    }
+};

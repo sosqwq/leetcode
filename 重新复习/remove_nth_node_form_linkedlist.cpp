f4bfacbeded0b1d09bcd96deaab23e6082a072ac
sosqwq <57452001+sosqwq@users.noreply.github.com>
2026-01-16 18:42:49 -0600

    Create remove_nth_node_form_linkedlist.cpp

---
+++ 重新复习/remove_nth_node_form_linkedlist.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct ListNode{
+    int val;
+    ListNode * next;
+    ListNode (): val(0), next(nullptr){};
+    ListNode (int x):val(x), next(nullptr) {};
+    ListNode (int x, ListNode *next) : val(x), next(next){}
+};
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode * dummyHead = new ListNode(0);
+        ListNode * slow = dummyHead;
+        ListNode * fast = dummyHead;
+
+        dummyHead -> next = head;
+        n++;
+        for (int i= 0; i< n+1; i++){
+            fast = fast -> next;
+        }
+
+        while (fast != nullptr){
+            fast = fast -> next;
+            slow = slow -> next;
+        }
+
+        slow -> next = slow -> next -> next;
+
+        return head;
+
+    }
+};

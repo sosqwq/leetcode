41125f207120c33c83d7256ca29d0f17e315b1a3
sosqwq <57452001+sosqwq@users.noreply.github.com>
2026-01-22 00:19:27 -0600

    Create reverse_string2.cpp

---
+++ 重新复习/string/reverse_string2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <cstring> 
+#include <unordered_set>
+#include <unordered_map>
+#include <algorithm>
+using namespace std;
+
+
+class Solution {
+public:
+    string reverseStr(string s, int k) {
+ 
+        for(int i= 0; i< s.size(); i+= 2*k){
+            if(i+k < s.size()){
+                reverse(s.begin() + i, s.begin() + i + k);
+            }
+            else{
+                reverse(s.begin() + i, s.end());
+            }
+        }
+
+    return s;
+    }
+};

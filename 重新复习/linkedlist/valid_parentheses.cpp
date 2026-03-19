2fdcb6f916a92b2cc6d256b607e47af4dd3300b5
sosqwq <57452001+sosqwq@users.noreply.github.com>
2026-03-18 23:51:59 -0500

    Create valid_parentheses.cpp

---
+++ 重新复习/linkedlist/valid_parentheses.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <cstring> 
+#include <unordered_set>
+#include <unordered_map>
+#include <algorithm>
+#include <queue>
+#include <stack>
+using namespace std;
+
+class Solution {
+    public:
+    bool isValid(string s) {
+        stack<char> stack;
+        if(s.size()%2 != 0) return false;
+        for(int i = 0; i< s.size(); i++){
+            if(s[i] == '('){
+                stack.push(')');
+            }
+            else if(s[i] == '{'){
+                stack.push('}');
+            }
+            else if(s[i] == '['){
+                stack.push(']');
+            }
+            else if( stack.empty() || s[i] != stack.top()){
+                return false;
+            }
+            else{
+                stack.pop();
+            }
+        }
+
+        return stack.empty();
+        }
+};

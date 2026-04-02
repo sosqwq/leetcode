a91d9f16a38a48969d0b6726cc7a73191d388725
sosqwq <57452001+sosqwq@users.noreply.github.com>
2026-04-01 23:59:11 -0500

    Create binarytree_postOrder_traversal.cpp

---
+++ 重新复习/binary_tree/binarytree_postOrder_traversal.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <cstring> 
+#include <unordered_set>
+#include <unordered_map>
+#include <algorithm>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode * left;
+    TreeNode * right;
+    TreeNode(): val(0),left(nullptr),right(nullptr){}
+};
+
+class Solution {
+public:
+    void traversal(TreeNode * current,vector<int>& vec){
+        if(current == nullptr) return;
+        traversal(current-> left, vec);
+        traversal(current-> right, vec);    
+        vec.push_back(current-> val);
+    }
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> result;
+        traversal(root, result);
+        return result;
+    }
+};

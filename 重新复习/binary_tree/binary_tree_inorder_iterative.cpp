d0b05185c37ba9ba7af1fa612460b22d0df59d67
sosqwq <57452001+sosqwq@users.noreply.github.com>
2026-04-14 00:50:07 -0500

    Update binary_tree_inorder_iterative.cpp

--- 重新复习/binary_tree/binary_tree_inorder_iterative.cpp
+++ 重新复习/binary_tree/binary_tree_inorder_iterative.cpp
@@ -18,8 +18,24 @@ struct TreeNode{
 
 class Solution {
 public:
-
+    
     vector<int> inorderTraversal(TreeNode* root) {
- 
+        vector<int> result;
+        stack<TreeNode*> st;
+        TreeNode * cur = root;
+        while( cur != NULL|| !st.empty()){
+            if(cur != NULL){
+                st.push(cur);
+                cur = cur->left;
+            }
+            else{
+                cur = st.top();
+                result.push_back(cur-> val);
+                cur = cur->right;
+                st.pop();
+            }
+
+        }
+        return result;
     }
 };
